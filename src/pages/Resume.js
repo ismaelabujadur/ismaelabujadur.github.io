7676cf4b43bb8f016a5dffa816d80f3e9f68bf6c
Ismael <ismael@3bweb.com>
2022-09-04 19:17:29 +0100

    Remove references from menu

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -19,13 +19,12 @@ const sections = [
   'Experience',
   'Skills',
   'Courses',
-  'References',
 ];
 
 const Resume = () => (
   <Main
     title="Resume"
-    description="Ismael Abujadur's Resume. Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet Labs, and Facebook."
+    description="Ismael Abujadur's Resume."
   >
     <article className="post" id="resume">
       <header>
